b2f39a9be552d5d69bc762e53b46ee73df6184d4
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-02-21 14:24:14 +0000

    endpont`{GET]/item` criado

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Worldddd')
 })
 
 app.get('/oi', function (req, res) {
     res.send('Ola mundo')
+  })
+  // Lista de Personagens
+  const lista = ['Rick Sanches','Morty Smith', 'Summer Smith']
+  
+  //Read all -> [Get]/item
+  app.get('/item', function(req, res){
+    res.send(lista)
   })
 
 app.listen(3000)
